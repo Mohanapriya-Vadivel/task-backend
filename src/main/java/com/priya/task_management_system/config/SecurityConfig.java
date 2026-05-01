d4795950d9c2e1fb541d3085330e2722c33ac782
Priya-glitch681 <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2026-05-01 14:34:56 +0530

    Fix CORS port

--- src/main/java/com/priya/task_management_system/config/SecurityConfig.java
+++ src/main/java/com/priya/task_management_system/config/SecurityConfig.java
@@ -46,7 +46,12 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://task-backend-dboo.onrender.com"));
+//        config.setAllowedOrigins(List.of("http://localhost:5173","https://task-backend-dboo.onrender.com"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "https://task-backend-dboo.onrender.com"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
